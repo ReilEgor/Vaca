7d16c520b77f75f771191963953e80ffd182badd
We1l6.f <we1l6.f@gmail.com>
2026-01-26 14:52:55 +0200

    feat: add interface statusRep for redis

--- services/CoordinatorService/internal/domain/interfaces.go
+++ services/CoordinatorService/internal/domain/interfaces.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"time"
 
 	outPkg "github.com/ReilEgor/Vaca/pkg"
 	"github.com/google/uuid"
@@ -13,4 +14,10 @@ type CoordinatorUsecase interface {
 	CreateTask(ctx context.Context, keywords []string, sources []string) (uuid.UUID, error)
 	GetVacancies(ctx context.Context, filter outPkg.VacancyFilter) ([]*outPkg.Vacancy, int64, error)
 	GetAvailableSources(ctx context.Context) ([]*outPkg.Source, int64, error)
+}
+
+//go:generate mockery --name StatusRepository --output ../mocks/domain --outpkg domain --case=underscore
+type StatusRepository interface {
+	Set(ctx context.Context, taskID string, status string, ttl time.Duration) error
+	Get(ctx context.Context, taskID string) (string, error)
 }
